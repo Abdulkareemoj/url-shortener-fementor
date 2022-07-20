2c3dc81c1433c40b67f8758c3427783615fa8a1d
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-20 21:17:01 +0100

    prep

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,28 @@
+import React from "react";
 
-export default function Blog() {
+import logo from '../images/logo.svg';
+
+export default function navbar() {
   return (
     <div className="container">
       <div className="content">
+        {logo}
    
       </div>
+      <header>
+				<nav className="">
+					<div>
+						<a href="#">WOMEN</a>
+						<a href="#">MEN</a>
+						<a href="#">KIDS</a>
+						<a className="o" href="#"></a>
+					</div>
+					<div className="box">
+						<img src={logo} alt="" srcset="" />
+					</div>
+
+				</nav>
+			</header>
     </div>
   );
 }
