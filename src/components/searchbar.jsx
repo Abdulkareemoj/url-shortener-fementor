fc40fab7e1c10b1ac33ae5b023f4ae388cbf8a4c
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-21 01:16:27 +0100

    add hero, search and nav components

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -3,16 +3,14 @@ import React from "react";
 
 export default function navbar() {
   return (
-    <div className="container">
-      <div className="content">
-     <h1>More than just shorter links</h1> 
-
-<h5>Build your brand’s recognition and get detailed insights 
-on how your links are performing.</h5>
-
-Get Started
-    </div>
+    <div className="App">
+  
+        <div className="main">
+          <div className="search">
+            <input type="text" placeholder="Search a blog" />
+          </div>
+          <p>Shorten it!</p>
+        </div>
     </div>
   );
 }
-
