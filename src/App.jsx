468cb7d67f170481f65e59d048165e9e5ace9a66
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-21 01:02:59 +0100

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import navbar from "./components/navbar"
+import navbar from "./components/navbar";
+import "./styles.scss";
 
 
 function App() {
   return (
+    <div className="App">
     <navbar />
+  </div>
   );
 }
 
