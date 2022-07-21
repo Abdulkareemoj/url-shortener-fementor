323e302a198ef4aa94c5c33019e7f20ce215bdc7
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-21 01:10:00 +0100

    add content

---
+++ src/components/searchbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function navbar() {
+  return (
+    <div className="container">
+      <div className="content">
+     <h1>More than just shorter links</h1> 
+
+<h5>Build your brand’s recognition and get detailed insights 
+on how your links are performing.</h5>
+
+Get Started
+    </div>
+    </div>
+  );
+}
+
