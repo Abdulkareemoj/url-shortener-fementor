87ed1bae4a3681a04834c47937297a72ff97bc7c
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-22 23:04:06 +0100

    add components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,58 @@
+
+  <footer class="footer">
+  <div class="container">
+      <a class="footerlogo" href="#">
+          <img src="images/logo.1.svg" alt="Easybank" srcset="" />
+        </a>
+
+        <div class="footersocial">
+<a href="#"><img src="/images/icon-facebook.svg" alt="Facebook" srcset=""></a>
+<a href="#"><img src="/images/icon-youtube.svg"  alt="Youtube" </a>
+<a href="#"><img src="/images/icon-twitter.svg" alt="Twitter" srcset=""></a>
+<a href="#"><img src="/images/icon-pinterest.svg" alt="Pinterest" srcset=""></a>
+<a href="#"><img src="/images/icon-instagram.svg" alt="Instagram" srcset=""></a>
+</div>
+
+
+<div class="footerlinks1">
+<a href="#">About Us</a>
+<a href="#">Contact</a>
+<a href="#">Blog</a>
+</div>
+
+<div class="footerlinks2">
+<a href="#">Careers</a>
+<a href="#">Support</a>
+<a href="#">Privacy Policy</a>
+</div>
+
+<div class="footercta">
+<a href="" class="button ">Request Invite</a>
+</div>
+
+<div class="footercopyright">  &copy; Easybank. All Rights Reserved 
+</div>
+</div>
+<div class="attribution">
+  Challenge by
+  <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https//:www.github.com/Abdulkareemoj">Abdulkareem Ojerinde</a>.
+</div>
+</footer>
+Features
+
+Link Shortening
+Branded Links
+Analytics
+
+Resources
+
+Blog
+Developers
+Support
+
+Company
+
+About
+Our Team
+Careers
+Contact
