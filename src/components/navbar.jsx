a15f89370cb9d958d8314c9600a38f6292f8c6db
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-22 11:09:48 +0100

    done with nav align for now

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,19 +4,19 @@ import logo from '../images/logo.svg';
 
 export default function navbar() {
   return (
-      <header className="container containerpall flex-sb flex-c flex-ai">
+      <header className="container containerpall flex-sb  flex-ai">
 				<nav className="header">
                 <div className="headerlinks">
                 <img className="headerlogo"src={logo} alt="logo"/>
-					
 						<a href="#">Features</a>
 						<a href="#">Pricing</a>
 						<a href="#">Resources</a>
-                       
+                        
+                        <div className="nav-right">
                     <a href="#">Login</a>
                     <a href="#" className="button">Sign Up</a>
                     </div>
-					
+					</div>
 					
     	</nav>
 			</header>
