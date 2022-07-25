07194cc19a5817202dbbb6b6188fac89c62937fb
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-07-25 21:13:08 +0100

    needs work

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
-import bg from "../images/bg-shorten-desktop.svg";
+
 
 export default function CTASection() {
   return (
   <section className="container">
         <div className="main">
-            <img src={bg} alt=""/>
+            
 <h1>Boost your links today</h1>
 
 <a href="#"className="button formobile">Get Started</a>
