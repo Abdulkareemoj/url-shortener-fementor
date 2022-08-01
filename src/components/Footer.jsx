b2082ac9cac5f3ab3ff30a8dba4dc2c1d28b33ed
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-08-01 21:13:52 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,95 @@ export default function Footer() {
   return (
     
   <footer className="footer" >
-  <div className="container">
+       <footer className="bg-slate-900 py-10 lg:py-20">
+        <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-5">
+          <article>
+            <img src={logo} alt="" />
+          </article>
+
+          <article>
+            <h3 className="text-white text-lg font-bold tracking-wide">
+              Features
+            </h3>
+            <ul>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">
+                  Link Shortening
+                </button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">
+                  Branded Links
+                </button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">
+                  Analytics
+                </button>
+              </li>
+            </ul>
+          </article>
+
+          <article>
+            <h3 className="text-white text-lg font-bold tracking-wide">
+              Resources
+            </h3>
+            <ul>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">Blog</button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">
+                  Developers
+                </button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">Support</button>
+              </li>
+            </ul>
+          </article>
+
+          <article>
+            <h3 className="text-white text-lg font-bold tracking-wide">
+              Company
+            </h3>
+            <ul>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">About</button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">
+                  Our Team
+                </button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">Careers</button>
+              </li>
+              <li>
+                <button className="text-slate-200 text-sm mt-1">Contact</button>
+              </li>
+            </ul>
+          </article>
+
+          <article>
+            <ul className="flex items-center">
+              <li>
+                <img src={Facebook} alt="" />
+              </li>
+              <li className="ml-4">
+                <img src={Twitter} alt="" />
+              </li>
+              <li className="mx-5">
+                <img src={Pinterest} alt="" />
+              </li>
+              <li>
+                <img src={Instagram} alt="" />
+              </li>
+            </ul>
+          </article>
+        </div>
+      </footer>
+  {/* <div className="container">
     <div><a className="footerlogo" href="#">
       <img src={logo} alt="logo" />
         </a></div>  
@@ -49,7 +137,8 @@ export default function Footer() {
 </div>
 
 
-</div><div className="footercopyright">  &copy; Easybank. All Rights Reserved 
+</div> */}
+<div className="footercopyright">  &copy; Easybank. All Rights Reserved 
 </div>
 <div className="attribution">
   Challenge by
