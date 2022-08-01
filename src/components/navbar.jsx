663d5affe2d2c23e93b6b879c1ba5fd36d5b8d88
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-08-01 21:06:54 +0100

    fix navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,77 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import { useState, React } from "react";
 
 import logo from '../images/logo.svg';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <header >
-            <nav className="pr-20 pl-20 px-20 py-20">
-             <div className="flex flex-row pl-2">  <img src={logo} alt="logo" />
-                    <a href="#">Features</a>
-                        <a href="#">Pricing</a>
-                        <a href="#">Resources</a></div>
-                       
-            
-                <div className="float-right">
-                    <a href="#">Login</a>
-                    <a href="#"  className="button">Sign Up</a>
-                </div>
-            </nav>
+        <header className="header max-width py-5">
+          <div className="flex items-center justify-between">
+            <article className="flex items-center">
+              <img src={logo} alt="" />
+  
+              <nav className="hidden md:block md:ml-5">
+                <ul className="flex items-start justify-start">
+                  <li>
+                    <button className="text-slate-400">Features</button>
+                  </li>
+                  <li className="my-5 md:my-0 md:mx-5">
+                    <button className="text-slate-400">Pricing</button>
+                  </li>
+                  <li>
+                    <button className="text-slate-400">Resources</button>
+                  </li>
+                </ul>
+              </nav>
+            </article>
+  
+            {isOpen && (
+              <div className="absolute left-5 right-5 top-20 rounded bg-slate-900 text-slate-200 text-center py-10 md:relative md:top-0 md:left-0 md:right-0 md:bg-transparent md:text-slate-700 md:text-left md:py-0 md:flex md:items-center">
+                <nav className="md:hidden">
+                  <ul className="flex flex-col items-center justify-center">
+                    <li>
+                      <button>Features</button>
+                    </li>
+                    <li className="my-5">
+                      <button>Pricing</button>
+                    </li>
+                    <li>
+                      <button>Resources</button>
+                    </li>
+                  </ul>
+                </nav>
+                <ul className="flex flex-col items-center justify-center">
+                  <li className="my-5">
+                    <button className="text-slate-400">Login</button>
+                  </li>
+                  <li>
+                    <button className="btn-cta rounded-full">Sign Up</button>
+                  </li>
+                </ul>
+              </div>
+            )}
+  
+            <div className="hidden md:block">
+              <ul className="flex items-center ml-5">
+                <li className="my-5 md:my-0 md:mr-5">
+                  <button className="text-slate-400">Login</button>
+                </li>
+                <li>
+                  <button className="btn-cta rounded-full">Sign Up</button>
+                </li>
+              </ul>
+            </div>
+  
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="uppercase text-sm tracking-wide md:hidden"
+            >
+              {isOpen ? "Close" : "Menu"}
+            </button>
+          </div>
         </header>
+
     );
-}
+  }
