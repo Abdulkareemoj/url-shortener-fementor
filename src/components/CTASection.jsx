f05fe4d8da3978dd7e75a6a2c41154e160956c69
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-08-02 21:06:50 +0100

    work on ctasection

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
-
+import bgdesktop from "../images/bg-boost-desktop.svg";
+import bgmobile from "../images/bg-boost-mobile.svg";
 
 export default function CTASection() {
   return (
-  <section className="container flex-ai">
-        <div className="maincta">
-            
-<h1 className="mainctatext">Boost your links today</h1>
+<section className="boost relative">
+        <picture>
+          <source media="(min-width: 768px)" srcSet={bgdesktop} />
+          <img src={bgmobile} alt="" />
+        </picture>
 
-<a href="#"className="button formobile">Get Started</a>
-</div>
-</section>
+        <div className="flex items-center justify-center flex-col boost-inner">
+          <h2 className="mb-5 text-3xl md:text-4xl text-white font-bold text-center">
+            Boost your links today
+          </h2>
+          <button className="btn-cta rounded-full">Get Started</button>
+        </div>
+      </section>
   );
 }
 
