71de2199fa0b21e373dadf87d8f2ad25abf5a63b
Abdulkareemoj <27707189+Abdulkareemoj@users.noreply.github.com>
2022-08-08 21:16:46 +0100

    fixed features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -6,38 +6,66 @@ import fullycus from '../images/icon-fully-customizable.svg';
 
 export default function Features() {
   return (
-    <section className="feature">
-      <div className="pr-20 pl-20 px-20 py-20">
-        <div className="featureintro">
-          <h2>Advanced Statistics</h2>
-          <div>
-            <img src={brandrec} alt="brandrec" />
-            <h2>Brand Recognition</h2>
-            <p>
-              Boost your brand recognition with each click. Generic links don’t
-              mean a thing. Branded links help instil confidence in your
-              content.
-            </p>
-          </div>
-          <div>
-            <img src={detailedrec} alt="detailedrec"/>
-            <h2>Detailed Records</h2>
-            <p>
-              Gain insights into who is clicking your links. Knowing when and
-              where people engage with your content helps inform better
-              decisions.
-            </p>
-          </div>
-          <div>
-            <img src={fullycus} alt="fullycus"/>
-            <h2>Fully Customizable</h2>
-            <p>
-              Improve brand awareness and content discoverability through
-              customizable links, supercharging audience engagement.
-            </p>
-          </div>
-        </div>
+    <section className="bg-gray-100 pt-32 pb-10 lg:pb-40">
+    <div className="max-width">
+      <h2 className="text-4xl font-bold text-slate-800 mb-3 text-center">
+        Advanced Statistics
+      </h2>
+      <p className="text-slate-400 text-center mb-10 lg:mb-20">
+        Track how your links are performing across the web <br /> with our
+        advanced statistics dashboard.
+      </p>
+
+      <div className="relative card grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        <div className="line" />
+        <article className="bg-white p-5 rounded relative">
+          <img
+            src={brandrec}
+            alt=""
+            className="p-3 -mt-12 bg-slate-800 rounded-full"
+          />
+          <h3 className="text-slate-800 text-lg mt-5 mb-2 font-bold">
+            Brand Recognition
+          </h3>
+          <p className="text-slate-400 text-sm">
+            Boost your brand recognition with each click. Generic links
+            don’t mean a thing. Branded links help instil confidence in your
+            content.
+          </p>
+        </article>
+
+        <article className="bg-white p-5 rounded relative">
+          <img
+            src={detailedrec}
+            alt=""
+            className="p-3 -mt-12 bg-slate-800 rounded-full"
+          />
+          <h3 className="text-slate-800 text-lg mt-5 mb-2 font-bold">
+            Detailed Records
+          </h3>
+          <p className="text-slate-400 text-sm">
+            Gain insights into who is clicking your links. Knowing when and
+            where people engage with your content helps inform better
+            decisions.
+          </p>
+        </article>
+
+        <article className="bg-white p-5 rounded relative">
+          <img
+            src={fullycus}
+            alt=""
+            className="p-3 -mt-12 bg-slate-800 rounded-full"
+          />
+          <h3 className="text-slate-800 text-lg mt-5 mb-2 font-bold">
+            Fully Customizable
+          </h3>
+          <p className="text-slate-400 text-sm">
+            Improve brand awareness and content discoverability through
+            customizable links, supercharging audience engagement.
+          </p>
+        </article>
       </div>
-    </section>
+    </div>
+  </section>
   );
 }
